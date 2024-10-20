85407696adca134d0e43a908d52edd125a3872ab
Nazar <kovtunnazik@gmail.com>
2024-10-20 23:52:27 +0300

    Update contacts.html, index.html, models.py: Small fix

--- web_app/my_site/news/models.py
+++ web_app/my_site/news/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Articles(models.Model):
-    title = models.CharField('Назва', max_length=50 )
+    title = models.CharField('Назва', max_length=50)
     anons = models.CharField('Анонс', max_length=250)
     full_text = models.TextField('Допис')
     date = models.DateTimeField('Дата публікації')
